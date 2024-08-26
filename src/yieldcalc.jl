ac8f80065519243de7f4127b87955cb499e030fa
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2024-08-26 15:09:16 +0200

    more fixes

--- src/yieldcalc.jl
+++ src/yieldcalc.jl
@@ -52,7 +52,7 @@ function _setup_conversion(ϕs_target, N, B, Zs)
         return 
     end
     function vjp!(vJ, v, μs, εs)
-        vJ .= (N * v)' * Diagonal(exp.(N * μs + B * εs) .* Zs) * N
+        jvp!(vJ, v, μs, εs)
         return 
     end
     return f!, jac!, jvp!, vjp!
