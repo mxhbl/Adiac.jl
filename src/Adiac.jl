16b55ce73767aee1d6e6a07394edec5004874383
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2025-05-14 17:05:30 +0200

    removed composition

--- src/Adiac.jl
+++ src/Adiac.jl
@@ -18,7 +18,6 @@ include("kinetics.jl")
 
 export linear_design, convex_design, convex_multidesign
 export logdensities, densities, monomer_densities, logyields, yields, μs_of_ϕs
-export composition, compositions
 export polyform_hessian, entropy
 export singleton_sets, count_faces
 export monoadd_kinetics
--- src/utils.jl
+++ src/utils.jl
@@ -28,39 +28,3 @@ end
 function infapprox(x, inf_val=99.9)
     return replace(x, Inf => inf_val, -Inf => -inf_val)
 end
-
-
-sigmas(ps::AbstractVector{<:Polyform}) = Roly.symmetry_number.(ps)
-
-function composition(p::Polyform, assembly_system::AssemblySystem)
-    n, k = size(assembly_system)
-    m = zeros(Int, n + k)
-
-    spcs = Roly.species(p)
-    for s in spcs
-        m[s] += 1
-    end
-
-    es = Graphs.edges(p.anatomy)
-    double_bonds = [e for e in es if reverse(e) in es]
-    bonds = []
-    for b in double_bonds
-        if b ∉ bonds && reverse(b) ∉ bonds
-            push!(bonds, b)
-        end
-    end
-
-    bondlist = findall(Roly.intmat(assembly_system))
-    filter!(x->x[1] <= x[2], bondlist)
-    sort!(bondlist)
-    
-    for b in bonds
-        lsrc, ldst = sort([p.anatomy.labels[b.src], p.anatomy.labels[b.dst]])
-        i = findfirst(x->x==CartesianIndex(lsrc, ldst), bondlist)
-        m[n + i] += 1
-    end
-
-    return m
-end
-compositions(ps::AbstractVector{<:Polyform}, sys::AssemblySystem) = reduce(vcat, composition.(ps, Ref(sys))')
-
